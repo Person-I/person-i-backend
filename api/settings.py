40560bc631acadc470bee818d5232ce6c4732b2a
Jacek Ciuba <ciubajacek@gmail.com>
2025-02-22 14:34:21 +0100

    Update static file paths for Vercel and Django deployment
    - Modify Vercel configuration to use 'staticfiles' directory
    - Update Django settings to adjust static file paths
    - Change WhiteNoise static file storage configuration

--- api/settings.py
+++ api/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
@@ -153,4 +153,4 @@ REST_FRAMEWORK = {
 }
 
 # Enable WhiteNoise compression and caching support
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
